16002d0e34a951d0016da03cc2e1e655724a321c
jakezien <jakezien@hey.com>
2023-09-29 13:25:38 -0400

    title size fix for mobile

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,9 +11,6 @@ const config: Config = {
       display: ["Garamond Pro Display", "serif"],
     },
     extend: {
-      fontSize: {
-        title: '6rem'
-      },
       colors: {
         hotPink: "#F22E60",
         crimson: "#D83027",
